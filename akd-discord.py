401b529524b1dbb2a3d81dc416047fc81503f437
kkerwin1 <kkerwin1@gmail.com>
2021-07-23 04:24:46 +0000

    Nightly commit 07/22/2021

--- akd-discord.py
+++ akd-discord.py
@@ -3,19 +3,31 @@ import discord
 tasks = discord.ext.tasks
 
 class DiscordManager(discord.Client):
+    """
+    Handles discord connection and messaging.
+    """
+
     def __init__(self, main):
         self.main = main        
         self.my_background_task.start()
     
     async def on_ready(self):
+        """
+        We've logged in.
+        """
+
         print("Logged in as")
         print(self.user.name)
         print(self.user.id)
         print("------")
     
-    @tasks.loop(seconds=60)
+    @tasks.loop(seconds=self.getSetting("pollingInterval"))
     async def my_background_task(self):
-        channel = self.get_channel(channel_id)
+        """
+        Run main loop.
+        """
+
+        channel = self.get_channel(self.getSetting("channelID"))
         try:
             msg = self.main.obtainUpdate()
             await channel.send(msg)
@@ -24,4 +36,15 @@ class DiscordManager(discord.Client):
     
     @my_background_task.before_loop
     async def before_my_task(self):
+        """
+        Tasks to run before each loop execution.
+        """
+
         await self.wait_until_ready()
+    
+    async def getSetting(self, setting):
+        """
+        Return live setting from settingsManager for specified setting.
+        """
+        
+        return self.main.settingsManager.settings[setting]
--- main.py
+++ main.py
@@ -8,8 +8,39 @@ from akd-files import FileManager
 from akd-settings import SettingsManager
 
 class Main(self):
+    """
+    Central management "hub" class that allows other managers to communicate w/one another.
+    """
+
     def __init__(self):
-        self.discordManager = DiscordManager(self)
-    
+        """
+        Structure
+        """
+
+        self.guiManager         = GuiManager(self)
+        self.fileManager        = FileManager(self)
+        self.settingsManager    = SettingsManager(self)
+        self.discordManager     = DiscordManager(self)
+            
     def run(self):
+        """
+        Start it
+        """
+
+        self.guiManager.run()
+        self.fileManager.run()
+        self.settingsManager.run()
         self.discordManager.run()
+    
+    def stop(self):
+        """
+        Stop it
+        """
+
+        self.discordManager.stop()
+        self.settingsManager.stop()
+        self.fileManager.stop()
+        self.guiManager.stop()
+
+main = Main()
+main.run()
