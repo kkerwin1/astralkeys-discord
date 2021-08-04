c3a7c2d348c209089b11f033cbbf68eaae26d9d0
kkerwin1 <kkerwin1@gmail.com>
2021-08-04 16:56:10 -0500

    Cleaned up/clarified comments. Added lib/akd-gui.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #   python >= 3.5.3
 #   discord.py; cf. https://discordpy.readthedocs.io/
 #   PyGithub; cf. github.com/PyGithub/PyGithub
+#   pyqt
 
 from lib.akd-discord import DiscordManager
 from lib.akd-gui import GuiManager
@@ -15,7 +16,8 @@ import traceback, sys, os
 
 class Main(self):
     """
-    Central management "hub" class that allows other managers to communicate w/one another.
+    Central management "hub" class that allows other managers to communicate
+    w/one another.
     """
 
     def __init__(self):
@@ -29,7 +31,7 @@ class Main(self):
         self.dataManager        = DataManager(self)
         self.settingsManager    = SettingsManager(self)
         self.discordManager     = DiscordManager(self)
-            
+
     def start(self):
         """
         Start it
@@ -41,7 +43,7 @@ class Main(self):
         self.dataManager.start()
         self.settingsManager.start()
         self.discordManager.start()
-    
+
     def stop(self):
         """
         Stop it
@@ -53,18 +55,22 @@ class Main(self):
         self.fileManager.stop()
         self.guiManager.stop()
         self.errorManager.stop()
-    
+
     def raiseError(self, traceback):
         """
-        Send a traceback report to ErrorManager, GuiManager, and ultimately GitHub
-        for creation of an issue when an error is found.
+        Send a traceback report to ErrorManager, GuiManager, and ultimately
+        GitHub for creation of an issue when an error is found.
         """
 
-        self.errorManager(traceback)
+        self.errorManager.raiseError(traceback)
 
 def main():
+    """
+    Main function. Start it, stop it, error it, drink tea.
+    """
+
     _main = Main()
-    try: 
+    try:
         _main.start()
     except ApplicationClose:
         _main.stop()
@@ -72,4 +78,9 @@ def main():
         _main.raiseError(traceback)
 
 if __name__ is __main__:
+    """
+    This is a script, not a library. It may not be imported into another script.
+    Won't work. Sorry!
+    """
+
     main()
